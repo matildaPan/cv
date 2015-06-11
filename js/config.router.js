ccb87126f5f0962ab8534a459cf52f0cddffa0b1
matildaPan <matilda.yipan@gmail.com>
2015-06-11 17:17:36 +1000

    change text add pictures

--- js/config.router.js
+++ js/config.router.js
@@ -4,25 +4,30 @@
  * Config for the router
  */
 angular.module('myApp')
-  .run(
-    [          '$rootScope', '$state', '$stateParams',
-      function ($rootScope,   $state,   $stateParams) {
-          $rootScope.$state = $state;
-          $rootScope.$stateParams = $stateParams;
-      }
-    ]
-  )
   .config(
-    [          '$stateProvider', '$urlRouterProvider',
+    ['$stateProvider', '$urlRouterProvider',
       function ($stateProvider,   $urlRouterProvider) {
           
           $urlRouterProvider
               .otherwise('/home');
           $stateProvider
-              .state('home', {
-                  url: '/home',
-                  templateUrl: 'pages/app.html'
-              });
+            .state('home', {
+                url: '/home',
+                templateUrl: 'pages/app.html',
+                abstract:true
+            })
+
+
+            .state('home.create', {
+              url: '',      //keep the url empty, so this will make it match the same url as it's parent state url, because it appends nothing to the parent url.
+              templateUrl: 'pages/blocks/create.html',
+            })
+          
+            .state('home.edit', {
+                url: '/ep',
+                templateUrl: 'pages/blocks/edit.html'
+            });
+
 
       }]
   );
