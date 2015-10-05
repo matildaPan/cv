2144dba00295bc6d18dda66ee84501a33d474407
Matilda Pan <matilda.yipan@gmail.com>
2015-10-05 22:38:23 +1100

    [reconstruct to ui-router]
    - "/home" to home.html
    - "/portfolio" to portfolio.html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-var app = angular.module('myApp', ['ui.bootstrap', 'duScroll'])
+var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'duScroll'])
 	.value('duScrollDuration', 2000)
 	.value('duScrollOffset', 90);
+
+app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("/home");
+
+	$stateProvider
+		.state('home', {
+			url: '/home', 
+			templateUrl: 'partials/home/home.html',
+			controller: 'homeController'
+		})
+		.state('portfolio', {
+			url: '/portfolio',
+			templateUrl: 'partials/portfolio/portfolio.html',
+			//controller: 'portfolioController'
+		});
+});
--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,4 @@
-app.controller('mainController', ['$scope', '$location', 'textService', function ($scope, $location, textService) {
-	// $scope.scrollTo = function (id) {
-	// 	//stop the URL from changing to #id
-	// 	var old = $location.hash();
-	// 	$location.hash(id);
-	// 	console.log($location.hash());
-	// 	$anchorScroll();
-	// 	$location.hash(old);
-	// };
-
+app.controller('homeController', ['$scope', '$location', 'textService', function ($scope, $location, textService) {
 	$scope.TextObj = textService.getTextData();
 }]);
 
