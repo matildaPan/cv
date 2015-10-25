b8c295123c0c8d9ba8997e852aa13d9609dc3d3a
Matilda Pan <matilda.yipan@gmail.com>
2015-10-25 14:16:37 +1100

    [Clean up style.css CSS Style-sheet]

--- js/controller.js
+++ js/controller.js
@@ -2,10 +2,12 @@ app.controller('navController', ['$scope', '$state ', function($scope, $state ){
 	
 }])
 
+// Home Page
 app.controller('homeController', ['$scope', '$location', 'textService', function ($scope, $location, textService) {
 	$scope.TextObj = textService.getTextData();
 }]);
 
+// Portfolio Page
 app.controller('portfolioController', ['$scope', '$location', 'textService', '$document', function ($scope, $location, textService, $document) {
 
 	$scope.$on('$includeContentLoaded', function(event, src) {
@@ -16,62 +18,18 @@ app.controller('portfolioController', ['$scope', '$location', 'textService', '$d
 	});
 }]);
 
+//Project Section in Portfolio page
 app.controller('projectsController', ['$scope', '$uibModal', function($scope, $uibModal){
 
 	$scope.viewDetail = function() {
 		$uibModal.open({
 			animation: true,
-			templateUrl: 'partials/portfolio/projectDetail.html',
-			//controller: 'ModalInstanceCtrl',
-			size: 'lg'
+			templateUrl: 'partials/portfolio/projectDetail.html'
 		});
 	};
 }]);
 
-
+//Contact Page
 app.controller('contactController', ['$scope', function($scope){
-	// var myCity = {
-	// 	city : 'Toronto',
-	// 	desc : 'This is the best city in the world!',
-	// 	lat : 43.7000,
-	// 	long : -79.4000
-	// };
-
-	// var mapOptions = {
-	// 	zoom: 4,
-	// 	center: new google.maps.LatLng(40.0000, -98.0000),
-	// 	mapTypeId: google.maps.MapTypeId.TERRAIN
-	// };
-
-	// $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-	// $scope.markers = [];
-	
-	// var infoWindow = new google.maps.InfoWindow();
-	
-	// var createMarker = function (info){
-		
-	// 	var marker = new google.maps.Marker({
-	// 		map: $scope.map,
-	// 		position: new google.maps.LatLng(info.lat, info.long),
-	// 		title: info.city
-	// 	});
-	// 	marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-		
-	// 	google.maps.event.addListener(marker, 'click', function(){
-	// 		infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-	// 		infoWindow.open($scope.map, marker);
-	// 	});
-		
-	// 	$scope.markers.push(marker);
-		
-	// }  
-
-	// createMarker(myCity);
-
-	// $scope.openInfoWindow = function(e, selectedMarker){
-	// 	e.preventDefault();
-	// 	google.maps.event.trigger(selectedMarker, 'click');
-	// }
 
 }])
