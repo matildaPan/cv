3bacccbe33fc0fc3367ba141ef84859badf0781b
Matilda Pan <matilda.yipan@gmail.com>
2015-12-12 18:20:20 +1100

    [Added content to project section]

--- js/controller.js
+++ js/controller.js
@@ -21,13 +21,31 @@ app.controller('portfolioController', ['$scope', '$location', 'textService', '$d
 
 //Project Section in Portfolio page
 app.controller('projectsController', ['$scope', '$uibModal', function($scope, $uibModal){
-
 	$scope.webDetail = function() {
 		$uibModal.open({
 			animation: true,
-			templateUrl: 'partials/portfolio/webDetail.html'
+			templateUrl: 'partials/portfolio/webDetail.html',
+			controller: 'projectInstanceCtrl'
 		});
 	};
+
+	$scope.surveyDetail = function() {
+		$uibModal.open({
+			animation: true,
+			templateUrl: 'partials/portfolio/surveyDetail.html',
+			controller: 'projectInstanceCtrl'
+		});
+	};
+
+
+}]);
+
+app.controller('projectInstanceCtrl', ['$scope', function($scope){
+	$scope.slidesInterval = 3000;
+	$scope.noWrapSlides = false;
+	$scope.webSlides = [{image: 'css/images/projects/laravel/create-page.jpg', text: 'Create Project'}, {image: 'css/images/projects/laravel/edit-page.jpg', text: 'Edit Page'}, {image: 'css/images/projects/laravel/output-page.jpg', text: 'Output Page'}, {image: 'css/images/projects/laravel/edit-single-project.jpg', text: 'Edit Single Project'}, {image: 'css/images/projects/laravel/group-bar-chart.jpg', text: 'Group Bar Chart'}, {image: 'css/images/projects/laravel/single-bar-chart.jpg', text: 'Single Bar Chart'},  {image: 'css/images/projects/laravel/stacked-bar-chart.jpg', text: 'Stacked Bar Chart'}];
+
+	$scope.surveySlides = [{image: 'css/images/projects/surveys/vaccination.jpg', text: 'Survey Example 1'}, {image: 'css/images/projects/surveys/sansom.jpg', text: 'Survey Example 2'}, {image: 'css/images/projects/surveys/hand_wash.jpg', text: 'Survey Example 3'}];
 }]);
 
 //Contact Page
