ece8537ad626e692f6e890e54796f5bd82319c6b
matildaPan <matilda.yipan@gmail.com>
2016-01-08 16:21:08 +1100

    [Configure Grunt, and modify CSS]

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,38 +3,65 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-   copy: {
-      html: {
-        files: [{ src: './index.html', dest: './dist/index.html' }]
+
+   // copy: {
+   //    html: {
+   //      files: [{ src: './index.html', dest: './dist/index.html' }]
+   //    }
+   //  },
+
+    // concat: {
+    //   dist: {
+    //       src:[
+    //           'js/app.js',
+    //           'js/service.js',
+    //           'js/controller.js',
+    //           'js/directives.js'
+    //       ],
+    //       desc: 'js/production.js',
+    //   }
+    // },
+
+    uglify: {
+      build: {
+          src:[
+              'js/app.js',
+              'js/service.js',
+              'js/controller.js',
+              'js/directives.js'
+          ],
+          dest: 'js/production.min.js'
       }
     },
-    useminPrepare: {
-      html: './dist/index.html',
-      options: {
-        dest: './dist'
-      }
-    },
-    usemin: {
-      html: ['./dist/index.html']
-    }
+
+    // useminPrepare: {
+    //   html: './dist/index.html',
+    //   options: {
+    //     dest: './dist'
+    //   }
+    // },
+    // usemin: {
+    //   html: ['./dist/index.html']
+    // }
  
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+//  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-usemin');
+//  grunt.loadNpmTasks('grunt-contrib-cssmin');
+//  grunt.loadNpmTasks('grunt-usemin');
 
   // Default task(s).
   grunt.registerTask('default', [
-    'copy:html',
-    'useminPrepare',
-    'concat',
+    //'copy:html',
+    //'useminPrepare',
+//    'concat',
     'uglify',
+    //'cssmin'
     //'cssmin',
-    'usemin'
+    //'usemin'
   ]);
 
 
